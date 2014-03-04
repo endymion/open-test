f14c9ec3261354a3aa9820a96fdc0cdd16fa5c62
Ryan Alyn Porter <rap@endymion.com>
2014-03-04 15:47:52 -0500

    Generated default Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenTest::Application.config.secret_token = 'c3e935db2fe4aebcf1eac461b0ae14705d4da8b179824b6394fdd71135627580a47b2115fb66a7423a5e9750c22b9559189db5d71344540fcea8e42e0d4bde17'
